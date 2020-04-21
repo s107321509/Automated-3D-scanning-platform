576b8d6f74fffb2053fe4171f7a68b18f9ecc3a3
youtengchiu <42413252+s107321509@users.noreply.github.com>
2020-04-21 13:49:12 +0800

    Update stack.py

--- stack.py
+++ stack.py
@@ -1,10 +1,8 @@
-def photo_stack(stack_step, step_cnt):
+def photo_stack(stack_step, stack_step_count):
 	from time import sleep
 	import RPi.GPIO as GPIO
 	import os
 	
-#CW = forward ,CCW = backward
-#1250 = 0.5cm
 	DIR=5
 	STEP=6
 	CW=0
@@ -13,25 +11,28 @@ def photo_stack(stack_step, step_cnt):
 	GPIO.setmode(GPIO.BCM)
 	GPIO.setup(DIR, GPIO.OUT)
 	GPIO.setup(STEP, GPIO.OUT)
-	delay=0.0001
 	
+	delay=0.0001
 	count=0
+	
 	GPIO.output(DIR, CCW)
 	for i in range(stack_step):
-		for x in range(step_cnt):
+		for x in range(stack_step_count):
 			GPIO.output(STEP, GPIO.HIGH)
 			sleep(delay)
 			GPIO.output(STEP, GPIO.LOW)
         		sleep(delay)
-		#os.system('./run.sh')
-		count+=step_cnt 
+		os.system('./run.sh')
+		count+=stack_step_count 
 		sleep(1)
+		
 	GPIO.output(DIR,CW)
 	for x in range(count):
         	GPIO.output(STEP, GPIO.HIGH)
         	sleep(delay)
         	GPIO.output(STEP, GPIO.LOW)
         	sleep(delay)
+		
 	sleep(.5)
 	GPIO.cleanup()
 
