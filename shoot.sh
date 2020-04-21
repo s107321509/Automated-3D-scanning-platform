e835d31640a2d890af26c7a543bfddc7de408110
youtengchiu <42413252+s107321509@users.noreply.github.com>
2020-04-21 13:54:50 +0800

    Update and rename run.sh to shoot.sh

--- shoot.sh
+++ shoot.sh
@@ -12,11 +12,7 @@ capture() {
     mkdir -p $FOLDER
     echo @___folder: [$FOLDER]
     echo @____image: [$IMG]
-    # 照相命名並分類保存
+    # 照片命名
     gphoto2 --capture-image --filename $IMG
-    # echo  'hello(date +%s)' > $IMG && mv $IMG $FOLDER
-    #read -t 1 || return
-    # echo sleep 3 seconds && sleep 3 && echo sleep done
 };
-
 capture # 持續拍照
