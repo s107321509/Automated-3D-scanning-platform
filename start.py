538e69ef7d271941c66f8c695051e06d1f0a672f
youtengchiu <42413252+s107321509@users.noreply.github.com>
2020-04-21 13:57:13 +0800

    Update start.py

--- start.py
+++ start.py
@@ -78,9 +78,6 @@ if(end > begin):
 print("\n\ndistance: {}mm".format((end-begin)/250))
 #how many steps from begin to end
 stack_step = input("\nstep: ")
-
 stack_step_count = (end-begin)/step
-
 GPIO.cleanup()
-
 tilting.turn(stack_step, stack_step_count)
