6a2da294711e741580bdf556fbf90af97e39eca6
youtengchiu <42413252+s107321509@users.noreply.github.com>
2020-04-21 13:43:15 +0800

    Update panning.py

--- panning.py
+++ panning.py
@@ -1,4 +1,4 @@
-def panning_rotation(panning_step, stack_step, step_cnt):
+def panning_rotation(panning_step, stack_step, stack_step_count):
   from time import sleep
   import RPi.GPIO as GPIO
   import os
@@ -8,23 +8,23 @@ def panning_rotation(panning_step, stack_step, step_cnt):
   STEP =14
   CW = 1
   CCW = 0
-  SPR = 1600/revnum
+  SPR = 1600/panning_step
 
   GPIO.setmode(GPIO.BCM)
   GPIO.setup(DIR, GPIO.OUT)
   GPIO.setup(STEP, GPIO.OUT)
   GPIO.output(DIR, CW)
   delay = .005
-
+  step_count=SPR
 
   for x in range(panning_step):
-    for y in range(SPR):
+    for y in range(step_count):
       GPIO.output(STEP, GPIO.HIGH)
       sleep(delay)
       GPIO.output(STEP, GPIO.LOW)
       sleep(delay)
     sleep(2)
-    stack.photo_stack(stack_step, step_cnt)
+    stack.photo_stack(stack_step, stack_step_count)
     #os.system('./run.sh')
     sleep(1)
     DIR =18 
